8bca3272cb5699bca12b1c1d7cae617b28f84d9b
Fuxia <fuxia150@protonmail.com>
2022-07-11 21:35:44 -0300

    New: load.py (get_name(), update_name() and parsing())

---
+++ load.py
@@ -0,0 +1,40 @@
+# Obter e atualizar o nome no arquivo de configuração JSON
+# Tratar os parâmetros passados pelo usuário
+
+import json
+import argparse
+
+PATH="config/config.json"
+
+def get_name():
+    
+    with open(PATH, "r") as file:
+        dict_json = json.load(file)
+        
+    return dict_json["name"]
+
+def update_name(new_name):
+    
+    dict_json = {"name":new_name}
+    
+    with open(PATH, "w") as file:
+        json.dump(dict_json, file, indent=2)
+        
+    return True
+        
+
+def parsing():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", help="get your name", type=str)
+    
+    args = parser.parse_args()
+    
+    if args.name:
+        return update_name(args.name)
+    
+    return False
+
+    
+    
+    
