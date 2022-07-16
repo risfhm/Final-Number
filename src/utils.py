bbff387d392363464f98749d8accaa0fff134885
Fuxia <fuxia150@protonmail.com>
2022-07-16 10:20:34 -0300

    utils.py: clear() and generate_msg() added

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
-# Utils
-
 from colorama import init, Fore, Back, Style
+from os import system
+from sys import platform
 
 init(autoreset=True)
 
@@ -15,23 +15,42 @@ TITLE="""
 # ----------------------------------------------
 
 
-MENU=f"""{Fore.GREEN} {TITLE} {Fore.WHITE}  
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+MENU=f"""{Fore.GREEN} {TITLE} {Fore.CYAN}
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-{Fore.YELLOW}
 
-Choose an option:
-
-[1] Casual Mode
-[2] Extreme Mode
-[3] Help
-[4] Exit
+[1] {Fore.CYAN}Casual Mode{Fore.YELLOW}
+[2] {Fore.CYAN}Extreme Mode{Fore.YELLOW}
+[3] {Fore.CYAN}Help{Fore.YELLOW}
+[4] {Fore.CYAN}Exit{Fore.YELLOW}
 """
 
 # ----------------------------------------------
 
-DIFF_MENU=f""">>>> {Fore.LIGHTGREEN_EX}Difficulty Menu <<<<
-{Fore.WHITE}
-[1] Easy 
-[2] Normal
-[3] Hard
-[4] Very Hard 
+DIFF_MENU=f"""{Fore.CYAN}>>>> {Fore.LIGHTGREEN_EX}Difficulty Menu {Fore.CYAN}<<<<
+{Fore.YELLOW}
+[1] {Fore.CYAN}Easy{Fore.YELLOW}
+[2] {Fore.CYAN}Normal{Fore.YELLOW}
+[3] {Fore.CYAN}Hard{Fore.YELLOW}
+[4] {Fore.CYAN}Very Hard{Fore.YELLOW}
 """
+
+def generate_msg(name, mode_name, diff_name, defeats):
+    return f"""[cyan bold][+][/cyan bold] [green]Congratulations, [bold]{name}[/bold]! Thank you for playing our considerably difficult game, you really 
+should be proud at this point. Well, can you repeat this feat?
+    
+[cyan bold][+][/cyan bold] [green]MODE: {mode_name}
+[cyan bold][+][/cyan bold] [green]DIFFICULTY: {diff_name}
+[cyan bold][+][/cyan bold] [green]FAILURE: {defeats}"""
+
+def clear():
+    
+    platforms = {
+        "linux":"clear",
+        "linux2":"clear",
+        "win32":"cls",
+        "darwin":"clear"
+    }
+    
+    if platform in platforms:
+        system(platforms[platform])
+    
